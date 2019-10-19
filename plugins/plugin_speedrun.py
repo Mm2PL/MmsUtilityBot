7b4d2f71c60a82ef93703256247df8152acb586d
Mm2PL <jakis128@gmail.com>
2019-10-19 15:22:38 +0200

    Create plugin_speedrun.py, do some MEGADANK stuff in twitch_auth.py, add srcomapi to requirements.txt, .gitignore changes

---
+++ plugins/plugin_speedrun.py
@@ -0,0 +1,227 @@
+#  This is a simple utility bot
+#  Copyright (C) 2019 Maciej Marciniak
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import typing
+
+import twitchirc
+
+try:
+    import main
+except ImportError:
+    import util_bot as main
+
+    exit()
+__meta_data__ = {
+    'name': 'plugin_speedrun',
+    'commands': [],
+}
+
+import srcomapi
+
+api = srcomapi.SpeedrunCom()
+current_games: typing.Dict[str, srcomapi.datatypes.Game] = {
+    # 'channel': srcomapi.datatypes.Game()
+}
+current_categories: typing.Dict[str, srcomapi.datatypes.Leaderboard] = {
+    # 'channel': srcomapi.datatypes.Leaderboard()
+}
+
+
+def _refresh_category(stream_title, channel, picked=None):
+    game = current_games[channel]
+    fitting = []
+    for cat in game.categories:
+        print(f'test cat {cat.name!r}, {stream_title!r}')
+        if cat.name in stream_title:
+            print('cat okay.')
+            fitting.append(cat.records[0])
+    if len(fitting) > 1:
+        if isinstance(picked, int):
+            current_categories[channel] = fitting[picked]
+            return True
+        elif isinstance(picked, str):
+            for i in fitting:
+                if i.category.name == picked:
+                    current_categories[channel] = i
+                    return True
+            for i in fitting:
+                if picked in i.category.name:
+                    current_categories[channel] = i
+                    return True
+    elif len(fitting) == 1:
+        current_categories[channel] = fitting[0]
+        return True
+    return False
+
+
+def _refresh_game(channel, picked=None):
+    stream, ret_code = main.twitch_auth.new_api.get_streams(user_login=channel, wait_for_result=True)
+    if ret_code == 200:
+        if 'data' in stream:
+            if len(stream['data']) == 0:
+                return False, 'No stream found.', 'api_no_stream'
+
+            data = stream['data'][0]
+            if data['type'] == '':
+                return False, f'monkaS {chr(128073)} api error', 'api_other'
+
+            game, ret_code2 = main.twitch_auth.new_api.get_games(id=data['game_id'])
+            game = game.json()
+            print(game)
+            if 'data' in game and len(game['data']):
+                # game data in form of
+                # {
+                #     'data': [
+                #         {
+                #             'id': 'game id',
+                #             'name': 'game name',
+                #             'box_art_url': 'url'
+                #         }
+                #     ]
+                # }
+                game = game['data'][0]
+                res = api.search(srcomapi.datatypes.Game, {'name': game['name']})
+                if len(res) > 1:
+                    if picked is None or (isinstance(picked, str) and picked.rstrip(' ') == ''):
+                        names = ", ".join([i.name for i in res])[::-1].replace(", "[::-1], " and "[::-1], 1)[::-1]
+                        # join every games' name with ", " and then replace the last ", " with " and ".
+
+                        return False, f'Found {len(res)} results on sr com. {names}.', 'multiple'
+
+                    if isinstance(picked, int):
+                        if picked - 1 > len(res) or picked - 1 < 0:
+                            return False, f'Cannot pick game number {picked}.', 'game_bad_id'
+
+                        r: srcomapi.datatypes.Game = res[picked - 1]
+                        current_games[channel] = r
+                        print('set', r)
+                        if channel in current_categories:
+                            del current_categories[channel]
+                    elif isinstance(picked, str):
+                        for i in res:
+                            if picked == i.name:
+                                current_games[channel] = i
+                                if channel in current_categories:
+                                    del current_categories[channel]
+                                print('set', i)
+                                break
+
+                        for i in res:
+                            if picked in i.name:
+                                current_games[channel] = i
+                                if channel in current_categories:
+                                    del current_categories[channel]
+                                print('set', i)
+                                break
+                elif len(res) == 1:
+                    current_games[channel] = res[0]
+                    print('set', res[0])
+                else:
+                    return False, 'Game not found, weird.', 'game_not_found'
+            cat_found = _refresh_category(data['title'], channel)
+            if cat_found:
+                return (True, f'{current_games[channel].name} and set category to '
+                              f'{current_categories[channel].category.name}',
+                        'update_both')
+            else:
+                return True, f"{current_games[channel].name}, couldn't update category", 'update_game'
+
+        else:
+            return False, f'monkaS {chr(128073)} API didn\'t return anything.', 'api_empty'
+
+    else:
+        return False, f'monkaS {chr(128073)} bad return code {ret_code!r}', 'api_code'
+
+
+def _format_time(time_):
+    minutes = time_ // 60
+    seconds = time_ % 60
+
+    hours = minutes // 60
+    minutes = minutes % 60
+    return (f'{int(hours)} hour{"s" if hours > 1 or hours == 0 else ""}, '
+            f'{int(minutes)} minute{"s" if minutes > 1 or minutes == 0 else ""}, '
+            f'{int(seconds)} second{"s" if seconds > 1 or seconds == 0 else ""}')
+
+
+@main.bot.add_command('worldrecord')
+def command_worldrecord(msg: twitchirc.ChannelMessage):
+    if msg.channel in current_categories:
+        category = current_categories[msg.channel]
+        time_ = category.runs[0]["run"].times['primary_t']
+        players = ' and '.join([p.name for p in category.runs[0]["run"].players])
+
+        main.bot.send(msg.reply(f'@{msg.user} Current WR for {current_games[msg.channel].name} '
+                                f'({category.category.name}) is '
+                                f'{_format_time(time_)} by {players}'))
+    else:
+        if msg.channel in current_games:
+            main.bot.send(msg.reply(f'@{msg.user}, There is a game set but there is no category set. Cannot fetch WR.'))
+        else:
+            main.bot.send(msg.reply(f'@{msg.user}, There is no game set on this channel. Ask a mod to run the '
+                                    f'update_game command.'))
+
+
+@main.bot.add_command('update_game', forced_prefix=None, enable_local_bypass=True,
+                      required_permissions=['wr.update_game'])
+def command_update_game(msg: twitchirc.ChannelMessage):
+    picked = (msg.text + ' ').split(' ', 1)[1].rstrip(' ')
+
+    if picked == '':
+        picked = None
+    elif picked.isnumeric():
+        picked = int(picked)
+
+    st, ret_msg, err_name = _refresh_game(msg.channel, picked=picked)
+    if st is False:
+        if err_name == 'multiple':
+            main.bot.send(msg.reply(f'@{msg.user} {ret_msg}. Use {command_update_game.chat_command} [number] '
+                                    f'or {command_update_game.chat_command} [game title]'
+                                    f'or {command_update_game.chat_command} [part of title] to select the game.'))
+        else:
+            main.bot.send(msg.reply(f'@{msg.user} Error: {ret_msg}'))
+    else:
+        main.bot.send(msg.reply(f'@{msg.user}, Okay, set game to {ret_msg}'))
+
+
+@main.bot.add_command('update_cat', forced_prefix=None, enable_local_bypass=True,
+                      required_permissions=['wr.update_cat'])
+def command_update_cat(msg: twitchirc.ChannelMessage):
+    if msg.channel not in current_games:
+        main.bot.send(msg.reply(f'@{msg.user} Cannot update category, there\'s no game set.'))
+        return
+    picked = (msg.text + ' ').split(' ', 1)[1]
+
+    if picked == '':
+        picked = None
+    elif picked.isnumeric():
+        picked = int(picked)
+    if picked is None:
+        stream, ret_code = main.twitch_auth.new_api.get_streams(user_login=msg.channel, wait_for_result=True)
+        if ret_code == 200:
+            if 'data' in stream:
+                data = stream['data'][0]
+                if data['type'] == '':
+                    main.bot.send(msg.reply(f'@{msg.user} monkaS {chr(127073)} API error.'))
+                    return
+                _refresh_category(stream['title'], msg.channel, picked)
+            else:
+                main.bot.send(msg.reply(f'@{msg.user} Stream not found.'))
+    else:
+        o = _refresh_category(picked, msg.channel, picked)
+        if o:
+            main.bot.send(msg.reply(f'@{msg.user} Okay, set category to {picked}'))
+        else:
+            main.bot.send(msg.reply(f'@{msg.user} error.'))
--- twitch_auth.py
+++ twitch_auth.py
@@ -15,6 +15,7 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import json
+import textwrap
 import time
 import typing
 
@@ -63,3 +64,321 @@ def save():
     if json_data != orig_data:
         with open('twitch_api.json', 'w') as f:
             json.dump(json_data, f, sort_keys=True, indent=4)
+
+
+class Auth:
+    def __init__(self):
+        with open('twitch_api.json', 'r') as f:
+            self.data = json.load(f)
+
+    @property
+    def client_id(self):
+        return self.data['client_id']
+
+    @property
+    def client_secret(self):
+        return self.data['client_secret']
+
+    @property
+    def token(self):
+        return self.data['access_token']
+
+
+class API:
+    def __init__(self, auth):
+        self.auth = auth
+        self.main_url = 'http://localhost'
+        self.headers = {
+            'Authorization': f'Bearer {self.auth.token}'
+        }
+        self.end_points = {
+            'end_point_name': {
+                'path': 'path/to/end_point',
+                'args': [
+                    'a',
+                    '[b]'
+                ],
+                'request_type': 'get'
+            }
+        }
+
+    def generate_functions(self):
+        for name, data in self.end_points.items():
+            args = []
+            args2 = []
+            print(name, data)
+            for arg in data['args']:
+                if arg.startswith('[') and arg.endswith(']'):
+                    args.append(arg[1:-1] + '=None')
+                    args2.append(f'{arg[1:-1]}={arg[1:-1]}')
+                else:
+                    args.append(arg)
+                    args2.append(f'{arg}={arg}')
+            code = textwrap.dedent(f'''
+            def func({", ".join(args)}, **kwargs):
+                return self._do_end_point({name!r}, {", ".join(args2)}, **kwargs)
+            ''')
+            variables = {
+                'self': self
+            }
+            exec(code, variables, variables)
+            setattr(self, name, variables['func'])
+
+    def _do_end_point(self, end_point_name, **kwargs):
+        if 'arg_type' not in self.end_points[end_point_name] or self.end_points[end_point_name]['arg_type'] == 'format':
+            return self.request(self.end_points[end_point_name]['path'].format(**kwargs),
+                                request_type=self.end_points[end_point_name]['request_type'])
+        elif self.end_points[end_point_name]['arg_type'] == 'parameter':
+            return self.request(self.end_points[end_point_name]['path'],
+                                request_type=self.end_points[end_point_name]['request_type'],
+                                **kwargs)
+
+    def request(self, end_point, *, request_type='get', wait_for_result=False, **kwargs):
+        """
+        Request something from the API.
+
+        :param end_point: Path to the end point
+        :param request_type: Request type, can be 'get', 'post', 'put', 'delete' or 'head'
+        :param wait_for_result: Wait for the result to come back. If this is True the deserialized data and the
+        status code
+        :param kwargs: Parameters to give to the request.
+        :return:
+        """
+        if request_type == 'get':
+            r = requests.get(self.main_url + end_point, headers=self.headers, params=kwargs)
+        elif request_type == 'post':
+            r = requests.post(self.main_url + end_point, headers=self.headers, params=kwargs)
+        elif request_type == 'put':
+            r = requests.put(self.main_url + end_point, headers=self.headers, params=kwargs)
+        elif request_type == 'delete':
+            r = requests.delete(self.main_url + end_point, headers=self.headers, params=kwargs)
+        elif request_type == 'head':
+            r = requests.head(self.main_url + end_point, headers=self.headers, params=kwargs)
+        else:
+            raise RuntimeError(f'Invalid request type {request_type!r}')
+        if wait_for_result:
+            return r.json(), r.status_code
+        else:
+            return r, -1
+
+
+class TwitchNewAPI(API):
+    def __init__(self, auth):
+        super().__init__(auth)
+        self.main_url = 'https://api.twitch.tv/helix/'
+        self.headers = {
+            'Authorization': f'Bearer {self.auth.token}',
+            'Client-ID': self.auth.client_id
+        }
+        self.end_points = {
+            'create_clip': {
+                'path': 'clips',
+                'args': [
+                    'broadcaster_id',
+                    '[has_delay]'
+                ],
+                'request_type': 'post',
+                'arg_type': 'parameter'
+            },
+            'get_clips': {
+                'arg_type': 'parameter',
+                'path': 'clips',
+                'args': [
+                    '[broadcaster_id]',
+                    '[game_id]',
+                    '[id]',
+
+                    '[after]',
+                    '[before]',
+                    '[ended_at]',
+                    '[first]',
+                    '[started_at]'
+                ],
+                'request_type': 'get'
+            },
+            'get_code_status': {
+                'arg_type': 'parameter',
+                'path': 'entitlements/codes',
+                'args': [
+                    'code',
+                    'user_id'
+                ],
+                'request_type': 'get'
+            },
+            'redeem_code': {
+                'arg_type': 'parameter',
+                'path': 'entitlements/code',
+                'args': [
+                    'code',
+                    'user_id'
+                ],
+                'request_type': 'post'
+            },
+            'get_top_games': {
+                'arg_type': 'parameter',
+                'path': 'games/top',
+                'args': [
+                    '[after]',
+                    '[before]',
+                    '[first]'
+                ],
+                'request_type': 'get'
+            },
+            'get_games': {
+                'arg_type': 'parameter',
+                'path': 'games',
+                'request_type': 'get',
+                'args': [
+                    '[id]',
+                    '[name]',
+                ]
+            },
+            'get_banned_users': {
+                'arg_type': 'parameter',
+                'path': 'moderation/banned',
+                'request_type': 'get',
+                'args': [
+                    'broadcaster_id',
+
+                    '[user_id]',
+                    '[after]',
+                    '[before]'
+
+                ]
+            },
+            'get_moderators': {
+                'arg_type': 'parameter',
+                'path': 'moderation/moderators',
+                'request_type': 'get',
+                'args': [
+                    'broadcaster_id',
+                    '[user_id]',
+                    '[after]'
+                ]
+            },
+            'get_streams': {
+                'arg_type': 'parameter',
+                'path': 'streams',
+                'request_type': 'get',
+                'args': [
+                    '[after]',
+                    '[before]',
+                    '[first]',
+                    '[game_id]',
+                    '[language]',
+                    '[user_id]',
+                    '[user_login]'
+                ]
+            },
+            'get_streams_metadata': {
+                'arg_type': 'parameter',
+                'path': 'streams/metadata',
+                'request_type': 'post',
+                'args': [
+                    '[after]',
+                    '[before]',
+                    '[first]',
+                    '[game_id]',
+                    '[language]',
+                    '[user_id]',
+                    '[user_login]'
+                ]
+            },
+            'get_stream_tags': {
+                'arg_type': 'parameter',
+                'path': 'streams/tags',
+                'request_type': 'get',
+                'args': [
+                    'broadcaster_id'
+                ]
+            },
+            'get_users': {
+                'arg_type': 'parameter',
+                'path': 'users',
+                'request_type': 'get',
+                'args': [
+                    '[id]',
+                    '[login]',
+                ]
+            },
+            'get_user_followers': {
+                'arg_type': 'parameter',
+                'path': 'users/follows',
+                'request_type': 'get',
+                'args': [
+                    '[after]',
+                    '[first]',
+                    '[from_id]',
+                    '[to_id]'
+                ]
+            },
+            'get_videos': {
+                'arg_type': 'parameter',
+                'path': 'videos',
+                'request_type': 'get',
+                'args': [
+                    '[id]',
+                    '[user_id]',
+                    '[game_id]',
+                    '[after]',
+                    '[before]',
+                    '[first]',
+                    '[language]',
+                    '[period]',
+                    '[sort]',
+                    '[type]'
+                ]
+            },
+        }
+
+
+class TwitchV5API(API):
+    def __init__(self, auth):
+        super().__init__(auth)
+        self.main_url = 'https://api.twitch.tv/kraken'
+        self.headers = {
+            'Authorization': f'OAuth {self.auth.token}',
+            'Client-ID': self.auth.client_id,
+            'Accept': 'application/vnd.twitchtv.v5+json'
+        }
+        self.end_points = {
+            # 'end_point_name': {
+            #     'path': 'path/to/end_point',
+            #     'args': [
+            #         'a',
+            #         '[b]'
+            #     ],
+            #     'request_type': 'get'
+            # }
+            'check_token': {
+                'path': '',
+                'args': [
+
+                ],
+                'request_type': 'get'
+            },
+            'get_cheermotes': {
+                'path': '/v5/bits/actions',
+                'args': [
+
+                ],
+                'request_type': 'get'
+            },
+            'get_channel_by_token': {
+                'path': '/channel',
+                'args': [],
+                'request_type': 'get'
+            },
+            'get_channel_by_id': {
+                'path': '/channels/{id}',
+                'args': [
+                    'id'
+                ],
+                'request_type': 'get'
+            },
+        }
+
+
+_auth = Auth()
+new_api = TwitchNewAPI(_auth)
+new_api.generate_functions()
