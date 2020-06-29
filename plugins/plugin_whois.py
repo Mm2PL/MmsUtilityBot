e1912aee4e87f508745b407a817eb16058cc12b5
Mm2PL <jakis128@gmail.com>
2020-06-29 15:08:12 +0200

    Update plugin
    changes:
      - use plugins.utils.arg_parser for argument parsing
      - more information,
      - hide bans behind +verbose

--- plugins/plugin_whois.py
+++ plugins/plugin_whois.py
@@ -14,22 +14,14 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import datetime
-import shlex
 import time
 import typing
 
 import aiohttp
-
-try:
-    # noinspection PyPackageRequirements
-    import main
-
-except ImportError:
-    import util_bot as main
-
-    exit()
 # noinspection PyUnresolvedReferences
 import twitchirc
+
+import util_bot.constants
 
 try:
     import plugin_plugin_manager as plugin_manager
@@ -41,55 +33,71 @@ try:
 except ImportError:
     import plugins.plugin_prefixes as plugin_prefixes
 
+import plugins.utils.arg_parser as arg_parser
+
 __meta_data__ = {
     'name': 'plugin_whois',
     'commands': [
         'whois'
     ]
 }
-log = main.make_log_function('whois')
-
-whois_parser = twitchirc.ArgumentParser(prog='whois')
-g = whois_parser.add_mutually_exclusive_group(required=True)
-g.add_argument('-i', '--id', help='Use the ID instead of using a username', dest='id', type=int)
-g.add_argument('-n', '--name', help='Use the username', dest='name', type=str)
-whois_parser.add_argument('-c', '--channels', help='Run in the context of CHANNELS', dest='channels', type=str)
-whois_parser.add_argument('-v', '--verbose', help='Load more information, runs ban check', dest='verbose',
-                          action='store_true')
+log = util_bot.make_log_function('whois')
 
 
 @plugin_manager.add_conditional_alias('whois', plugin_prefixes.condition_prefix_exists)
-@main.bot.add_command('mb.whois')
-async def command_whois(msg: twitchirc.ChannelMessage):
+@util_bot.bot.add_command('mb.whois')
+async def command_whois(msg: util_bot.StandardizedMessage):
     if not bots or bots_invalidates <= time.time():
         await _load_bots()
 
-    cd_state = main.do_cooldown('whois', msg, global_cooldown=30,
-                                local_cooldown=60)
+    cd_state = util_bot.do_cooldown('whois', msg, global_cooldown=30,
+                                    local_cooldown=60)
     if cd_state:
         return
     try:
-        argv = shlex.split(msg.text.replace('\U000e0000', ''))
-    except ValueError as e:
-        return f'@{msg.user} FeelsWeirdMan {e.args}'
+        args = arg_parser.parse_args(util_bot.delete_spammer_chrs(msg.text), {
+            'id': int,
+            'name': str,
+            'bans': bool,
+            'channels': str,
 
-    args = whois_parser.parse_args(argv[1:] if len(argv) > 1 else [])
-    if args is None:
-        return f'@{msg.user} {whois_parser.format_usage()}'
-    if args.id is not None:
-        name = args.id
+            'verbose': bool,
+
+            1: str
+        }, defaults={
+            'id': None,
+            'name': None,
+
+            'bans': False,
+            'channels': '',
+
+            'verbose': False,
+
+            1: None
+        })
+    except arg_parser.ParserError as err:
+        return f'@{msg.user}, {err.message}'
+    print(args)
+
+    if args['name'] and args[1]:
+        return f'@{msg.user}, Name argument provided twice.'
+    if args[1]:
+        args['name'] = args[1]
+
+    if args['id'] is not None:
+        name = args['id']
         id_ = True
-    elif args.name is not None:
-        name = args.name
+    elif args['name'] is not None:
+        name = args['name']
         id_ = False
     else:
-        return f'@{msg.user} Do you really want the bot to crash?'
+        return (f'@{msg.user} {msg.text.split(" ")[0]} (name:TWITCH_USERNAME|id:TWITCH_ID) [+verbose] '
+                f'[channels:CHANNELS]. OR {msg.text.split(" ")[0]} TWITCH_USERNAME [+verbose] [channels:CHANNELS]')
 
     async with aiohttp.request('get', f'https://api.ivr.fi/twitch/resolve/{name}',
                                params=({'id': 1}) if id_ else {},
                                headers={
-                                   'User-Agent': 'Mm\'sUtilityBot/v1.0 (by Mm2PL), Twitch chat bot',
-                                   'Requested-By': msg.user
+                                   'User-Agent': util_bot.constants.USER_AGENT
                                }) as request:
         data = await request.json()
         if data['status'] == 404:
@@ -106,9 +114,11 @@ async def command_whois(msg: twitchirc.ChannelMessage):
             roles += 'staff, '
         if roles == '':
             roles = 'none'
+
         roles = (roles[::-1].replace(', '[::-1], '', 1))[::-1]
         # replace last ", " with "".
-        if data['displayName'].lower() != data['login'].lower():
+
+        if data['displayName'].casefold() != data['login'].casefold():
             login = f'({data["login"]})'
         else:
             login = ''
@@ -121,10 +131,18 @@ async def command_whois(msg: twitchirc.ChannelMessage):
             created_at_str += i
 
         created_on = datetime.datetime.strptime(created_at_str, '%Y-%m-%dT%H:%M:%S')
+        logo_warning = ''
+        if data['logo'].startswith('https://static-cdn.jtvnw.net/user-default-pictures'):
+            logo_warning = 'avatar: DEFAULT, '
+
         bot_notes = ''
         bot = _find_bot(data['login'])
         if bot is not None:
-            last_active = f', Last active: {bot["lastSeen"][:-4]}' if bot['lastSeen'] is not None else ''
+            if args['verbose']:
+                last_active = f', Last active: {bot["lastSeen"][:-4]}' if bot['lastSeen'] is not None else ''
+            else:
+                last_active = ''
+
             bot_notes = (f' Prefix: {bot["prefix"] if bot["prefix"] is not None else "<blank>"}, '
                          f'Description: {bot["description"] if bot["description"] is not None else "<blank>"}, '
                          f'Language: {bot["language"] if bot["language"] is not None else "<unknown>"}'
@@ -132,20 +150,20 @@ async def command_whois(msg: twitchirc.ChannelMessage):
 
         banned_in = []
 
-        if args.channels:
-            if args.channels == 'no':
+        if args['channels']:
+            if args['channels'] == 'no':
                 banned_check_channels = []
             else:
-                banned_check_channels = args.channels.split(',')
+                banned_check_channels = args['channels'].split(',')
         else:
-            if args.verbose:
-                banned_check_channels = main.bot.channels_connected.copy()
+            if args['verbose']:
+                banned_check_channels = util_bot.bot.channels_connected.copy()
             else:
                 banned_check_channels = []
 
         for ch in banned_check_channels.copy():
             if ch == 'joined':
-                banned_check_channels.extend(main.bot.channels_connected)
+                banned_check_channels.extend(util_bot.bot.channels_connected)
                 break
 
         for ch in banned_check_channels:
@@ -164,24 +182,26 @@ async def command_whois(msg: twitchirc.ChannelMessage):
                     expiration_note = f'lasts until {banned_data["expiresAt"].replace("T", " ").replace("Z", " UTC")}'
                 banned_in.append(ch + f'(since {banned_data["createdAt"].replace("T", " ").replace("Z", " UTC")},'
                                       f'{expiration_note})')
+
         print(banned_in)
         if banned_in:
-            banned_str = 'They are banned in ' + ('#' + ', #'.join(banned_in))
+            banned_str = '. They are banned in ' + ('#' + ', #'.join(banned_in))
+        elif banned_check_channels:
+            banned_str = '. They are banned in no known channels'
+        elif args['verbose']:
+            banned_str = '. Didn\'t check for bans'
         else:
-            if banned_check_channels:
-                banned_str = 'They are banned in no known channels'
-            else:
-                banned_str = 'Didn\'t check for bans'
+            banned_str = ''
 
         return (f'@{msg.user}, {"BANNED " if data["banned"] else ""}{"bot " if data["bot"] else ""}'
-                f'user {data["displayName"]}{login}, '
-                f'chat color: {data["chatColor"]}, '
+                f'user {data["displayName"]}{login}, {logo_warning}'
+                f'chat color: {data["chatColor"] if data["chatColor"] else "GRAY-NAME"}, '
                 f'account created at {created_on}, '
                 f'roles: {roles}, '
                 f'id: {data["id"]}, '
                 f'bio: '
-                f'{data["bio"] if data["bio"] is not None else "<blank>"}'
-                f'{bot_notes.rstrip(" ,.")}. {banned_str}.')
+                f'{data["bio"].rstrip(" ,.") if data["bio"] is not None else "<blank>"}. '
+                f'{bot_notes.rstrip(" ,.")}{banned_str}.')
 
 
 bots: typing.List[dict] = []
@@ -191,10 +211,10 @@ bots_invalidates = time.time()
 async def _load_bots():
     global bots, bots_invalidates
     bots_invalidates = time.time() + 600  # make the bot list invalidate in 10 minutes.
-    async with aiohttp.request('get', 'https://supinic.com/api/bot/active') as r:
+    async with aiohttp.request('get', 'https://supinic.com/api/bot-program/bot/list') as r:
         r: aiohttp.ClientResponse
         if r.status == 200:
-            bots = (await r.json())['data']
+            bots = (await r.json())['data']['bots']
             return 'ok'
         else:
             print(r.content)
