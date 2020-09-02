baf1da0f34bf460191393a717476deb32ec18b95
Mm2PL <jakis128@gmail.com>
2020-09-02 18:03:06 +0200

    Add suggestions ID descending endpoint

--- web/suggestions.py
+++ web/suggestions.py
@@ -14,7 +14,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import sqlalchemy
-from flask import abort, jsonify
+from flask import jsonify
 from sqlalchemy import text
 
 try:
@@ -187,6 +187,89 @@ def init(register_endpoint, ipc_conn, main_module, session_scope):
                     for suggestion in suggestions
                 ]
 
+            })
+
+    @register_endpoint('/suggestions/list/desc/<int:page>')
+    def list_all_suggestions_newest_first(page):
+        """
+        List suggestions. Sorted by ID descending.
+
+        :param page: Page number you want. Pages are 50 or less entries in size, indexed from 0.
+
+        .. start-raw
+        <h3>Returns</h3>
+        <ol>
+            <li>
+                <b>page (Number)</b> page number you are on.
+            </li>
+            <li>
+                <b>page_size (Number)</b> maximum size of a page.
+            </li>
+            <li>
+                <b>count (Number)</b> total number of suggestions.
+            </li>
+            <li>
+                <b>data (Object[])</b> data
+                <ul>
+                    <li>
+                        <b>text (String)</b> text of the suggestion
+                    </li>
+                    <li>
+                        <b>notes (String)</b> notes from the suggestion
+                    </li>
+                    <li>
+                        <b>state (String)</b> suggestion state, can be 'new', 'not_a_suggestion', 'rejected', 'accepted'
+                    </li>
+                    <li>
+                        <b>creation_date (String)</b> ISO representation of the date,
+                        format string used is "%Y-%m-%dT%H:%M%SZ"
+                    </li>
+                    <li>
+                        <b>author (Object)</b> object representing the author of the suggestion
+                        <ul>
+                            <li>
+                                <b>id</b> Twitch user id of the account
+                            </li>
+                            <li>
+                                <b>name</b> Last known user name of this account.
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ol>
+        .. stop-raw
+        """
+        with main_module.session_scope() as session:
+            suggestion_query = (session.query(Suggestion)
+                                .filter(Suggestion.is_hidden == False))
+            count = suggestion_query.count()
+            suggestions = (suggestion_query
+                           .order_by(Suggestion.id.desc())
+                           .offset(page * main_module.PAGE_SIZE)
+                           .limit(main_module.PAGE_SIZE)
+                           .all()
+                           )
+            return jsonify({
+                'status': 200,
+                'page': page,
+                'page_size': main_module.PAGE_SIZE,
+                'count': count,
+                'data': [
+                    {
+                        'text': suggestion.text,
+                        'notes': suggestion.notes,
+                        'state': suggestion.state.name,
+                        'id': suggestion.id,
+                        'creation_date': (suggestion.creation_date.strftime("%Y-%m-%dT%H:%M%SZ")
+                                          if suggestion.creation_date is not None else None),
+                        'author': {
+                            'id': suggestion.author.twitch_id,
+                            'name': suggestion.author.last_known_username
+                        }
+                    }
+                    for suggestion in suggestions
+                ]
             })
 
     @register_endpoint('/suggestions/stats')
