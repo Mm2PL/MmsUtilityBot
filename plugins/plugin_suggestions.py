28a2d8560e2134ea66c3631fba77e818da11126d
Mm2PL <miau@mail.kotmisia.pl>
2021-06-10 21:59:09 +0200

    legacy

--- plugins/plugin_suggestions.py
+++ plugins/plugin_suggestions.py
@@ -57,6 +57,7 @@ except ImportError:
 
     exit(1)
 import plugins.models.suggestion as suggestion_model
+
 __meta_data__ = {
     'name': 'suggestions',
     'commands': []
@@ -105,8 +106,8 @@ def command_check_suggestion(msg: twitchirc.ChannelMessage):
             if suggestion is None:
                 return f'@{msg.user} Suggestion id {target!r} not found.'
             else:
-                main.bot.send(msg.reply(f'@{msg.user} '
-                                        f'{suggestion.humanize(suggestion.author.last_known_username == msg.user)}'))
+                return (f'@{msg.user} '
+                        f'{suggestion.humanize(suggestion.author.last_known_username == msg.user)}')
     else:
         with main.session_scope() as session:
             user = main.User.get_by_message(msg, no_create=True)
@@ -117,8 +118,8 @@ def command_check_suggestion(msg: twitchirc.ChannelMessage):
                            .filter(Suggestion.state.notin_([Suggestion.SuggestionState.done,
                                                             Suggestion.SuggestionState.rejected,
                                                             Suggestion.SuggestionState.not_a_suggestion])))
-            main.bot.send(msg.reply(f'@{msg.user} Your suggestions: '
-                                    f'{", ".join([f"{s.id} ({s.nice_state()})" for s in suggestions])}'))
+            return (f'@{msg.user} Your suggestions: '
+                    f'{", ".join([f"{s.id} ({s.nice_state()})" for s in suggestions])}')
 
 
 @plugin_help.add_manual_help_using_command('Mark a suggestion as resolved.',
@@ -139,9 +140,8 @@ def command_resolve_suggestion(msg: twitchirc.ChannelMessage):
     if t[2] in state_names:
         state = Suggestion.SuggestionState[t[2]]
     else:
-        main.bot.send(msg.reply(f'@{msg.user}, Invalid state: {t[2]!r}. Choose between '
-                                f'{", ".join([repr(i.name) for i in Suggestion.SuggestionState])}'))
-        return
+        return (f'@{msg.user}, Invalid state: {t[2]!r}. Choose between '
+                f'{", ".join([repr(i.name) for i in Suggestion.SuggestionState])}')
     if len(t) == 4:
         notes = t[3]
     else:
@@ -152,173 +152,6 @@ def command_resolve_suggestion(msg: twitchirc.ChannelMessage):
         if notes is not None:
             suggestion.notes = notes
         session.add(suggestion)
-    main.bot.send(msg.reply(f'@{msg.user} Modified suggestion id {target!r}, '
-                            f'new state {state}, '
-                            f'new notes {notes}.'))
-
-
-PAGE_SIZE = 50
-
-
-@plugin_ipc.add_command('get_user_suggestions')
-def _ipc_command_list_suggestions(sock: socket.socket, msg: str, socket_id):
-    arg: str
-    _, arg = msg.replace('\n', '').replace('\r\n', '').split(' ', 1)
-    args = arg.split(' ', 1)
-    user_id = None
-    page_num = None
-    if len(args) >= 2:
-        if args[0].isnumeric():
-            user_id = int(args[0])
-        if args[1].isnumeric():
-            page_num = int(args[1])
-    if user_id is None or page_num is None:
-        return (b'!1\r\n'
-                b'~Invalid usage.\r\n'
-                +
-                plugin_ipc.format_json(
-                    {
-                        'type': 'error',
-                        'source': 'get_user_suggestions',
-                        'message': 'Invalid usage.'
-                    })
-                )
-    else:
-        def _fetch_suggestions(rwq):
-            print(f'fetch {user_id} p {page_num}')
-            with main.session_scope() as session:
-                suggestions = (session.query(Suggestion)
-                               .filter(Suggestion.author_alias == user_id)
-                               .offset(page_num * PAGE_SIZE)
-                               .limit(PAGE_SIZE)
-                               .all())
-                rwq.put(
-                    plugin_ipc.format_json(
-                        {
-                            'type': 'suggestion_list',
-                            'page': page_num,
-                            'page_size': PAGE_SIZE,
-                            'data': [
-                                {
-                                    'text': suggestion.text,
-                                    'notes': suggestion.notes,
-                                    'state': suggestion.state.name,
-                                    'id': suggestion.id
-                                }
-                                for suggestion in suggestions
-                            ]
-                        }
-                    )
-                )
-
-        t = threading.Thread(target=_fetch_suggestions, args=(plugin_ipc.response_write_queues[socket_id],))
-        t.start()
-
-
-@plugin_ipc.add_command('get_suggestion')
-def _ipc_command_list_suggestions(sock: socket.socket, msg: str, socket_id):
-    arg: str
-    _, arg = msg.replace('\n', '').replace('\r\n', '').split(' ', 1)
-    args = arg.split(' ', 1)
-    suggestion_id = None
-    if len(args) >= 1:
-        if args[0].isnumeric():
-            suggestion_id = int(args[0])
-    if suggestion_id is None:
-        return (b'!1\r\n'
-                b'~Invalid usage.\r\n'
-                +
-                plugin_ipc.format_json(
-                    {
-                        'type': 'error',
-                        'source': 'get_suggestion',
-                        'message': 'Invalid usage.'
-                    })
-                )
-    else:
-        def _fetch_suggestions(rwq):
-            print(f'fetch suggestion {suggestion_id}')
-            with main.session_scope() as session:
-                suggestion = (session.query(Suggestion)
-                              .filter(Suggestion.id == suggestion_id)
-                              .first())
-                rwq.put(
-                    plugin_ipc.format_json(
-                        {
-                            'type': 'suggestion_list',
-                            'page': 0,
-                            'page_size': 1,
-                            'data':
-                                ([
-                                     {
-                                         'text': suggestion.text,
-                                         'notes': suggestion.notes,
-                                         'state': suggestion.state.name,
-                                         'id': suggestion.id,
-                                         'author': {
-                                             'name': suggestion.author.last_known_username,
-                                             'alias': suggestion.author.id
-                                         }
-                                     }
-                                 ] if suggestion is not None else [])
-                        }
-                    )
-                )
-
-        t = threading.Thread(target=_fetch_suggestions, args=(plugin_ipc.response_write_queues[socket_id],))
-        t.start()
-
-
-@plugin_ipc.add_command('get_suggestions')
-def _ipc_command_list_suggestions(sock: socket.socket, msg: str, socket_id):
-    arg: str
-    _, arg = msg.replace('\n', '').replace('\r\n', '').split(' ', 1)
-    args = arg.split(' ', 1)
-    page_num = None
-    if len(args) >= 1:
-        if args[0].isnumeric():
-            page_num = int(args[0])
-    if page_num is None:
-        return (b'!1\r\n'
-                b'~Invalid usage.\r\n'
-                +
-                plugin_ipc.format_json(
-                    {
-                        'type': 'error',
-                        'source': 'get_user_suggestions',
-                        'message': 'Invalid usage.'
-                    })
-                )
-    else:
-        def _fetch_suggestions(rwq):
-            print(f'fetch suggestions, p {page_num}')
-            with main.session_scope() as session:
-                suggestions = (session.query(Suggestion)
-                               .offset(page_num * PAGE_SIZE)
-                               .limit(PAGE_SIZE)
-                               .all())
-                rwq.put(
-                    plugin_ipc.format_json(
-                        {
-                            'type': 'suggestion_list',
-                            'page': page_num,
-                            'page_size': PAGE_SIZE,
-                            'data': [
-                                {
-                                    'text': suggestion.text,
-                                    'notes': suggestion.notes,
-                                    'state': suggestion.state.name,
-                                    'id': suggestion.id,
-                                    'author': {
-                                        'name': suggestion.author.last_known_username,
-                                        'alias': suggestion.author.id
-                                    }
-                                }
-                                for suggestion in suggestions
-                            ]
-                        }
-                    )
-                )
-
-        t = threading.Thread(target=_fetch_suggestions, args=(plugin_ipc.response_write_queues[socket_id],))
-        t.start()
+    return (f'@{msg.user} Modified suggestion id {target!r}, '
+            f'new state {state}, '
+            f'new notes {notes}.')
