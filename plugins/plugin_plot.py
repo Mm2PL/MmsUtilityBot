b32429d0c7fa137bf0889178696e6a421d89e005
Mm2PL <miau@mail.kotmisia.pl>
2021-10-06 16:19:56 +0200

    comment out inputenc and add wasysym

--- plugins/plugin_plot.py
+++ plugins/plugin_plot.py
@@ -795,11 +795,12 @@ class Plugin(util_bot.Plugin):
 
 
 LATEX_MINIMAL_DOC_FORMAT = r'''\documentclass[preview]{standalone}
-\usepackage[utf8x]{inputenc}
+%\usepackage[utf8x]{inputenc}
 \usepackage{ucs}
 \usepackage{fontspec}
 \usepackage[version=4]{mhchem}
 \usepackage{xcolor}
+\usepackage{wasysym}
 
 \newcommand{\darkmode}[1]{
     \colorbox{black}{
